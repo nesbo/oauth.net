18f17549691c7b3021540e83c9ce574a1007e488
Aleksandar Andonovski <aleksandar.andonovski@gmail.com>
2024-11-08 20:12:07 +0100

    Added Identity Server with Asp.NET Identity

---
+++ src/Bones.OAuth.Net/Bones.OAuth.Net/ServiceCollectionExtension.cs
@@ -0,0 +1,14 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bones.OAuth.Net;
+
+public static class ServiceCollectionExtension
+{
+    public static IServiceCollection AddOAuthClient(this IServiceCollection services)
+    {
+        
+        
+        
+        return services;
+    }
+}
